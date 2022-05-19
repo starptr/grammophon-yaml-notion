82f80d22bd7fdf5045932283a9f1af0c080816fc
Yuto Nishida <yuto@berkeley.edu>
2022-05-19 04:37:26 -0700

    Saner lint and impl all except req

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
   ],
   rules: {
     'no-unused-vars': 'warn',
+    'no-restricted-syntax': 'off',
+    'no-await-in-loop': 'off',
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -26,15 +26,79 @@ interface Work {
 interface Playlist {
   [playlistName: string]: Work[];
 }
-type Season = 'spring' | 'summer' | 'autumn' | 'winter'
+const seasons = ['spring', 'summer', 'autumn', 'winter'] as const;
+type Season = typeof seasons[number];
 interface YamlData {
   [year: string]: {
     // eslint-disable-next-line no-unused-vars
     [season in Season]: Playlist[];
   };
 }
+
+/*
+Parse a comma-separated string of artists into an array
+*/
+function parseArtists(str: string): string[] {
+  const padded = str.split(',');
+  const artists = padded.map((paddedStr) => paddedStr.trim());
+  return artists;
+}
+
+/*
+`works` should be ordered from top to bottom
+*/
+async function pushDataFromWorks(playlistName: string, works: Work[]) {
+  let dbWkOrderIdx = 100;
+  for (const work of works) {
+    const title: string = work.title || work.album;
+    const isAlbum: boolean = work.album !== undefined && work.album !== null;
+    const artists: string[] = parseArtists(work.artist);
+    const youtube: string = work.links.youtube || '';
+    const spotify: string = work.links.spotify || '';
+    const tiktok: boolean = !!work.links.tiktok;
+    const douyin: boolean = !!work.links.douyin;
+    const meme: boolean = !!work.links.meme;
+    const classical: boolean = !!work.links.classical;
+    // TODO: push work payload
+    dbWkOrderIdx += 100;
+  }
+}
+
+async function reqAddPlaylist(year: number, season: Season, dbPlOrderIdx: number, playlistName: string) {
+  // TODO:
+}
+
+/*
+`playlists` should be ordered from bottom to top.
+*/
+async function pushDataFromPlaylists(year: number, season: Season, playlists: Playlist[]) {
+  let dbPlOrderIdx = 10;
+  for (const playlist of playlists) {
+    const [[playlistName, works]] = Object.entries(playlist);
+    await reqAddPlaylist(year, season, dbPlOrderIdx, playlistName);
+    await pushDataFromWorks(playlistName, works);
+    dbPlOrderIdx += 10;
+  }
+}
+
+/*
+Year in DB is ordered from earliest to most recent
+Works are ordered from top to bottom
+*/
 async function pushDataFromYaml(yamlData: YamlData) {
-  console.log(yamlData['2021'].winter[0]['a well-deserved break']);
+  const years = Object.entries(yamlData);
+  years.sort((year1, year2) => (year1[0] < year2[0] ? -1 : 1));
+  await Promise.all(years.map(async (yearDataPair) => {
+    const [yearStr, yearData] = yearDataPair;
+    const year = parseInt(yearStr, 10);
+    await Promise.all(seasons.map(async (season) => {
+      if (season in yearData) {
+        const playlists = yearData[season];
+        playlists.reverse(); // process lower playlist first
+        await pushDataFromPlaylists(year, season, playlists);
+      }
+    }));
+  }));
 }
 await pushDataFromYaml(data);
 
@@ -65,4 +129,4 @@ async function addItem(txt: string) {
   }
 }
 
-//await addItem('my text');
+// await addItem('my text');
