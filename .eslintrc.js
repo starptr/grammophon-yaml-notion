b25644e12a5458321415db781451dac913355ec5
Yuto Nishida <yuto@berkeley.edu>
2022-05-19 03:01:34 -0700

    Fix preprocessing configs (templatable)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,18 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    es2017: true,
   },
   extends: [
     'airbnb-base',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: [
     '@typescript-eslint',
   ],
   rules: {
+    'no-unused-vars': 'warn',
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,42 @@
-import 'dotenv/config'
-import { Client, isNotionClientError } from "@notionhq/client"
-import YAML from 'yaml'
-import fs from 'fs'
+import 'dotenv/config';
+import { Client, isNotionClientError } from '@notionhq/client';
+import * as YAML from 'yaml';
+import * as fs from 'fs';
 
 const notion = new Client({ auth: process.env.NOTION_KEY });
 const dbId = process.env.NOTION_DB_GWORKS_ID;
 
 const legacyFile = fs.readFileSync('./data/grammophon.yaml', 'utf8');
 const data = YAML.parse(legacyFile);
-console.log(data);
+
+interface Work {
+  title?: string;
+  album?: string;
+
+  artist: string;
+  links: {
+    youtube?: string;
+    spotify?: string;
+    tiktok?: boolean;
+    douyin?: boolean;
+    meme?: boolean;
+    classical?: boolean;
+  }
+}
+interface Playlist {
+  [playlistName: string]: Work[];
+}
+type Season = 'spring' | 'summer' | 'autumn' | 'winter'
+interface YamlData {
+  [year: string]: {
+    // eslint-disable-next-line no-unused-vars
+    [season in Season]: Playlist[];
+  };
+}
+async function pushDataFromYaml(yamlData: YamlData) {
+  console.log(yamlData['2021'].winter[0]['a well-deserved break']);
+}
+await pushDataFromYaml(data);
 
 async function addItem(txt: string) {
   try {
@@ -19,22 +47,22 @@ async function addItem(txt: string) {
           title: [
             {
               text: {
-                content: txt
-              }
+                content: txt,
+              },
             },
-          ]
-        }
-      }
+          ],
+        },
+      },
     });
     console.log(response);
-    console.log("Nice this worked");
+    console.log('Nice this worked');
   } catch (error: unknown) {
     if (isNotionClientError(error)) {
-      console.log("Notion error: ", error);
+      console.log('Notion error: ', error);
     } else {
-      console.log("General error: ", error);
+      console.log('General error: ', error);
     }
   }
 }
 
-await addItem("my text");
+//await addItem('my text');
