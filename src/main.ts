5b80e198d046e7cec7fa8e60147abd44cdc7579f
Yuto Nishida <yuto@berkeley.edu>
2022-05-20 01:18:11 -0700

    Final

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import * as YAML from 'yaml';
 import throttledQueue from 'throttled-queue';
 import * as fs from 'fs';
 
-const throttle = throttledQueue(1, 350);
+const throttle = throttledQueue(1, 500);
 
 const notion = new Client({ auth: process.env.NOTION_KEY });
-const dbId = process.env.NOTION_DB_GWORKS_ID;
+const worksDbId = process.env.NOTION_DB_GWORKS_ID;
+const plistsDbId = process.env.NOTION_DB_PLISTS_ID;
 
 const legacyFile = fs.readFileSync('./data/grammophon.yaml', 'utf8');
 const data = YAML.parse(legacyFile);
@@ -17,9 +18,10 @@ interface Work {
   album?: string;
 
   artist: string;
-  links: {
+  links?: {
     youtube?: string;
     spotify?: string;
+    soundcloud?: string;
     tiktok?: boolean;
     douyin?: boolean;
     meme?: boolean;
@@ -47,6 +49,71 @@ function parseArtists(str: string): string[] {
   return artists;
 }
 
+interface WorkProps {
+  playlistName: string;
+  dbWkOrderIdx: number;
+  name: string;
+  artists: string[];
+  youtube: string | null;
+  spotify: string | null;
+  soundcloud: string | null;
+  album: boolean;
+  tiktok: boolean;
+  douyin: boolean;
+  meme: boolean;
+  classical: boolean;
+}
+async function reqAddWork(params: WorkProps) {
+  const plData = await throttle(() => notion.databases.query({
+    database_id: plistsDbId,
+    filter: {
+      property: 'name',
+      title: {
+        equals: params.playlistName,
+      },
+    },
+  }));
+  const plId = plData.results[0].id;
+  const res = await throttle(() => notion.pages.create({
+    parent: { database_id: worksDbId },
+    properties: {
+      playlists: {
+        relation: [{
+          id: plId,
+        }],
+      },
+      'work order': {
+        number: params.dbWkOrderIdx,
+      },
+      name: {
+        title: [{
+          text: { content: params.name },
+        }],
+      },
+      artists: {
+        multi_select: params.artists.map((artist) => ({
+          name: artist,
+        })),
+      },
+      YouTube: {
+        url: params.youtube,
+      },
+      Spotify: {
+        url: params.spotify,
+      },
+      SoundCloud: {
+        url: params.soundcloud,
+      },
+      album: { checkbox: params.album },
+      TikTok: { checkbox: params.tiktok },
+      Douyin: { checkbox: params.douyin },
+      meme: { checkbox: params.meme },
+      classical: { checkbox: params.classical },
+    },
+  }));
+  return res;
+}
+
 /*
 `works` should be ordered from top to bottom
 */
@@ -56,19 +123,59 @@ async function pushDataFromWorks(playlistName: string, works: Work[]) {
     const title: string = work.title || work.album;
     const isAlbum: boolean = work.album !== undefined && work.album !== null;
     const artists: string[] = parseArtists(work.artist);
-    const youtube: string = work.links.youtube || '';
-    const spotify: string = work.links.spotify || '';
-    const tiktok: boolean = !!work.links.tiktok;
-    const douyin: boolean = !!work.links.douyin;
-    const meme: boolean = !!work.links.meme;
-    const classical: boolean = !!work.links.classical;
-    // TODO: push work payload
+    const youtube: string = work.links?.youtube || null;
+    const spotify: string = work.links?.spotify || null;
+    const soundcloud: string = work.links?.soundcloud || null;
+    const tiktok: boolean = !!work.links?.tiktok;
+    const douyin: boolean = !!work.links?.douyin;
+    const meme: boolean = !!work.links?.meme;
+    const classical: boolean = !!work.links?.classical;
+    const workProps: WorkProps = {
+      playlistName,
+      dbWkOrderIdx,
+      name: title,
+      artists,
+      youtube,
+      spotify,
+      soundcloud,
+      album: isAlbum,
+      tiktok,
+      douyin,
+      meme,
+      classical,
+    };
+    await reqAddWork(workProps);
     dbWkOrderIdx += 100;
   }
 }
 
 async function reqAddPlaylist(year: number, season: Season, dbPlOrderIdx: number, playlistName: string) {
-  // TODO:
+  const res = await throttle(() => notion.pages.create({
+    parent: { database_id: plistsDbId },
+    properties: {
+      name: {
+        title: [
+          {
+            text: {
+              content: playlistName,
+            },
+          },
+        ],
+      },
+      year: {
+        number: year,
+      },
+      season: {
+        select: {
+          name: season,
+        },
+      },
+      'pl order': {
+        number: dbPlOrderIdx,
+      },
+    },
+  }));
+  return res;
 }
 
 /*
@@ -91,45 +198,65 @@ Works are ordered from top to bottom
 async function pushDataFromYaml(yamlData: YamlData) {
   const years = Object.entries(yamlData);
   years.sort((year1, year2) => (year1[0] < year2[0] ? -1 : 1));
-  await Promise.all(years.map(async (yearDataPair) => {
+  for (const yearDataPair of years) {
     const [yearStr, yearData] = yearDataPair;
     const year = parseInt(yearStr, 10);
-    await Promise.all(seasons.map(async (season) => {
+    for (const season of seasons) {
       if (season in yearData) {
         const playlists = yearData[season];
         playlists.reverse(); // process lower playlist first
         await pushDataFromPlaylists(year, season, playlists);
       }
-    }));
-  }));
-}
-await pushDataFromYaml(data);
-
-async function addItem(txt: string) {
-  try {
-    const response = await notion.pages.create({
-      parent: { database_id: dbId },
-      properties: {
-        title: {
-          title: [
-            {
-              text: {
-                content: txt,
-              },
-            },
-          ],
-        },
-      },
-    });
-    console.log(response);
-    console.log('Nice this worked');
-  } catch (error: unknown) {
-    if (isNotionClientError(error)) {
-      console.log('Notion error: ', error);
-    } else {
-      console.log('General error: ', error);
     }
   }
+  //await Promise.all(years.map(async (yearDataPair) => {
+  //  const [yearStr, yearData] = yearDataPair;
+  //  const year = parseInt(yearStr, 10);
+  //  await Promise.all(seasons.map(async (season) => {
+  //    if (season in yearData) {
+  //      const playlists = yearData[season];
+  //      playlists.reverse(); // process lower playlist first
+  //      await pushDataFromPlaylists(year, season, playlists);
+  //    }
+  //  }));
+  //}));
 }
+console.log(await pushDataFromYaml(data));
 
-// await addItem('my text');
+// async function addItem(txt: string) {
+//   try {
+//     const response = await notion.pages.create({
+//       parent: { database_id: worksDbId },
+//       properties: {
+//         title: {
+//           title: [
+//             {
+//               text: {
+//                 content: txt,
+//               },
+//             },
+//           ],
+//         },
+//       },
+//     });
+//     console.log(response);
+//     console.log('Nice this worked');
+//   } catch (error: unknown) {
+//     if (isNotionClientError(error)) {
+//       console.log('Notion error: ', error);
+//     } else {
+//       console.log('General error: ', error);
+//     }
+//   }
+// }
+// 
+// // await addItem('my text');
+// 
+// async function seeDb() {
+//   const res = await notion.databases.query({
+//     database_id: worksDbId,
+//   });
+//   console.log(res.results.map((page) => page.properties['work order']));
+// }
+// 
+// // await seeDb();
